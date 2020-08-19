d96e89d2f09fd74c2447e6b01dbfbd1a6fa78bb7
gaurav-dogra <gauru.dogra@gmail.com>
2020-08-19 01:03:32 +0100

    Third stage in progress

--- Auth.java
+++ Auth.java
@@ -1,9 +1,32 @@
 package advisor;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Auth extends SpotifyPlayerAction {
     @Override
     public void act() {
-        System.out.print("https://accounts.spotify.com/authorize?client_id=4a6790355b57464dadabd7a7285e6d0f&redirect_uri=http://localhost:8080&response_type=code\n" +
-                "---SUCCESS---\n");
+        createHttpServer();
+        System.out.print("https://accounts.spotify.com/authorize?client_id=" +
+                "4a6790355b57464dadabd7a7285e6d0f&redirect_uri=http://localhost:8080&response_type=code\n" +
+                "waiting for code...\n");
+    }
+
+    private void createHttpServer() {
+        try {
+            HttpServer server = HttpServer.create();
+            server.bind(new InetSocketAddress(8080), 0);
+            server.createContext("/", httpExchange -> {
+                String hello = "hello, world";
+                httpExchange.sendResponseHeaders(200, hello.length());
+                httpExchange.getResponseBody().write(hello.getBytes());
+                httpExchange.getResponseBody().close();
+            });
+            server.start();
+        } catch (IOException e) {
+            System.out.println("Error while creating HTTP Server");
+        }
     }
 }
